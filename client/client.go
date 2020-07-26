780ee525e6ded0383116b7aa17407c0096bdbe88
Marius <marius.feteanu@gmail.com>
2020-07-26 16:59:28 +0100

    some channel per user

--- client/client.go
+++ client/client.go
@@ -1,64 +1,64 @@
 package main
 
 import (
-    "net/http"
-    "fmt"
-    "log"
-    "strings"
-    "bufio"
-    "os"
+	"bufio"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 )
 
 func read_line(r *bufio.Reader) string {
-    line, err := r.ReadString('\n')
-    if err == nil {
-        return strings.TrimSuffix(line, "\n")
-    } else {
-        return ""
-    }
+	line, err := r.ReadString('\n')
+	if err == nil {
+		return strings.TrimSuffix(line, "\n")
+	} else {
+		return ""
+	}
 }
 
 func main() {
-    in := bufio.NewReader(os.Stdin)
-    log.Println("Starting client")
-    client := &http.Client{}
+	in := bufio.NewReader(os.Stdin)
+	log.Println("Starting client")
+	client := &http.Client{}
 
-    fmt.Print("username: ")
-    var username string
-    username = read_line(in)
-    fmt.Printf("<<%v>>\n", username)
+	fmt.Print("username: ")
+	var username string
+	username = read_line(in)
+	fmt.Printf("<<%v>>\n", username)
 
-    var to string
+	var to string
 
-    for {
-        var message string
-        fmt.Print("~ ")
-        message = read_line(in)
-        if message == "/q" {
-            break
-        }
+	for {
+		var message string
+		fmt.Print("~ ")
+		message = read_line(in)
+		if message == "/q" {
+			break
+		}
 
-        if strings.HasPrefix(message, "/t") {
-            to = message[3:]
-            fmt.Println(fmt.Sprintf("# sending to: <<%v>>", to))
-            continue
-        }
+		if strings.HasPrefix(message, "/t") {
+			to = message[3:]
+			fmt.Println(fmt.Sprintf("# sending to: <<%v>>", to))
+			continue
+		}
 
-        if to == "" {
-            fmt.Println("Try /t some_user_name, to talk to someone")
-            continue
-        }
+		if to == "" {
+			fmt.Println("Try /t some_user_name, to talk to someone")
+			continue
+		}
 
-        post_url := "http://localhost:8080/messages"
+		post_url := "http://localhost:8080/messages"
 
-        req, _ := http.NewRequest("POST", post_url, strings.NewReader(message))
-        req.Header.Add("Chat-From", username)
-        req.Header.Add("Chat-To", to)
+		req, _ := http.NewRequest("POST", post_url, strings.NewReader(message))
+		req.Header.Add("Chat-From", username)
+		req.Header.Add("Chat-To", to)
 
-        _, err := client.Do(req)
-        if err != nil {
-            log.Println(err)
-        }
-        fmt.Println(post_url)
-    }
+		_, err := client.Do(req)
+		if err != nil {
+			log.Println(err)
+		}
+		fmt.Println(post_url)
+	}
 }
