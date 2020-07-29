eb95b44c38ec8b70ae67850e9c350fd279059d4a
Marius Feteanu <marius.feteanu@icgam.com>
2020-07-29 20:00:02 +0100

    reafactor server

--- server/server.go
+++ server/server.go
@@ -21,6 +21,72 @@ type userMessage struct {
 	Content string
 }
 
+func getIncomingChannel(userChannels *sync.Map, u string) (userChannel chan userMessage) {
+	hopeUserChannel, exists := userChannels.Load(u)
+	if !exists {
+		userChannel = make(chan userMessage, 100)
+		userChannels.Store(u, userChannel)
+	} else {
+		userChannel = hopeUserChannel.(chan userMessage)
+	}
+	return userChannel
+}
+
+func dispatchUserIncomingMessages(userChannels *sync.Map, u string, w http.ResponseWriter) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("disconnected (receive goroutine): " + u)
+		}
+	}()
+
+	f, _ := w.(http.Flusher)
+	f.Flush()
+
+	for {
+		userChannel := getIncomingChannel(userChannels, u)
+		msg := <-userChannel
+		jsonBytes, _ := json.Marshal(msg)
+		_, err := w.Write(jsonBytes)
+		if err == nil {
+			f.Flush()
+		} else {
+			log.Println("disconnected : " + u)
+			return
+		}
+	}
+}
+
+func sendMessage(userChannels *sync.Map, from string, to string, message []byte) {
+	userChannel := getIncomingChannel(userChannels, to)
+	userChannel <- userMessage{from, string(message)}
+}
+
+func keepAlive(u string, w http.ResponseWriter) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("disconnected (receive goroutine): " + u)
+		}
+	}()
+
+	f, _ := w.(http.Flusher)
+	f.Flush()
+
+	for {
+		jsonBytes, jsonError := json.Marshal(nil)
+		if jsonError != nil {
+			log.Println(jsonError)
+		}
+		_, err := w.Write(jsonBytes)
+		if err == nil {
+			f.Flush()
+		} else {
+			log.Println("disconnected (heartbeat) : " + u)
+			return
+		}
+		time.Sleep(time.Second)
+	}
+}
+
 func main() {
 	log.Println("Starting server")
 
@@ -28,74 +94,18 @@ func main() {
 
 	http.HandleFunc("/messages/", func(w http.ResponseWriter, r *http.Request) {
 		pathParts := strings.Split(r.URL.Path, "/")
-		username := pathParts[len(pathParts)-1]
+		user := pathParts[len(pathParts)-1]
 
 		if r.Method == "POST" {
 			to := r.Header.Get("Chat-To")
-			log.Println("[" + username + "] -> [" + to + "]")
-			messageBytes, _ := ioutil.ReadAll(r.Body)
-			msg := message{
-				username,
-				to,
-				string(messageBytes)}
-
-			var userChannel chan userMessage
-			hopeUserChannel, exists := userChannels.Load(msg.To)
-			if !exists {
-				userChannel = make(chan userMessage, 100)
-				userChannels.Store(msg.To, userChannel)
-			} else {
-				userChannel = hopeUserChannel.(chan userMessage)
-			}
-			userChannel <- userMessage{msg.From, msg.Content}
+			message, _ := ioutil.ReadAll(r.Body)
+			sendMessage(&userChannels, user, to, message)
 		}
 
 		if r.Method == "GET" {
-			log.Println("getting messages for " + username)
-			flusher, _ := w.(http.Flusher)
-			flusher.Flush()
-
-			go func() {
-				defer func() {
-					if r := recover(); r != nil {
-						log.Println("disconnected (receive goroutine): " + username)
-					}
-				}()
-				var userChannel chan userMessage
-				hopeUserChannel, exists := userChannels.Load(username)
-				if !exists {
-					userChannel = make(chan userMessage, 100)
-					userChannels.Store(username, userChannel)
-				} else {
-					userChannel = hopeUserChannel.(chan userMessage)
-				}
-				for {
-					msg := <-userChannel
-					jsonBytes, _ := json.Marshal(msg)
-					_, err := w.Write(jsonBytes)
-					if err == nil {
-						flusher.Flush()
-					} else {
-						log.Println("disconnected : " + username)
-						return
-					}
-				}
-			}()
-
-			for {
-				jsonBytes, jsonError := json.Marshal(nil)
-				if jsonError != nil {
-					log.Println(jsonError)
-				}
-				_, err := w.Write(jsonBytes)
-				if err == nil {
-					flusher.Flush()
-				} else {
-					log.Println("disconnected (heartbeat) : " + username)
-					return
-				}
-				time.Sleep(time.Second)
-			}
+			log.Println("getting messages for " + user)
+			go dispatchUserIncomingMessages(&userChannels, user, w)
+			keepAlive(user, w)
 		}
 	})
 
